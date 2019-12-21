66c8611ca9abdd8c6b79785ac2e0664c3e1a14e6
Blake Bourque <techplex.engineer@gmail.com>
2019-12-20 20:55:26 -0500

    Bedroom FW

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -15,7 +15,7 @@ struct eeprom_data
 const int PIN_ZC_IN   = D5;
 
 
-const int numOutputs =2;
+const int numOutputs = 1;
 int outputPins[] = {D6, D4, D3, D2, D1, D0};
 
 E131 e131;
@@ -31,6 +31,5 @@ void setup()
 void loop()
 {
   e131.parsePacket();
-  ZCDimmer::getInstance()->setBrightness(0, e131.data[2]);
-  ZCDimmer::getInstance()->setBrightness(1, e131.data[3]);
+  ZCDimmer::getInstance()->setBrightness(0, e131.data[1]);
 }
